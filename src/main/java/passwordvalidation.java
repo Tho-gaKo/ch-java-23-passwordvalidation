4aea87c7f2bea2d1c3a2845d73d1248e57115261
Tho-gaKo <thomas-gabriel.korak@coop.ch>
2023-11-07 09:56:14 +0100

    Contains8letters and contains8letterstest
    started with number and test for number

--- src/main/java/passwordvalidation.java
+++ src/main/java/passwordvalidation.java
@@ -1,2 +1,58 @@
+import java.util.Scanner;
+
+
 public class passwordvalidation {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in); //Create scanner
+        System.out.println(" Enter a password ");
+
+        String password = sc.nextLine(); //Read user input
+        System.out.println(" Password is: " + password);
+
+
+    }
+    public static boolean passwordIs8letters(String password) {
+        String sevenLetter = "TheCook";
+
+        boolean is8Letters = false;
+
+        if (sevenLetter.length() < password.length()) {
+            is8Letters = true;
+
+        }
+        if (is8Letters) {
+            return true;
+        }
+    return false;
+    }
+    public static boolean passwordContainsNumber(String password) {
+        String hasNumber = "benching405";
+
+        boolean containsNumber = false;
+
+
+        if (containsNumber.){
+            containsNumber = true;
+        }
+        if (containsNumber) {
+            return true;
+        }
+        return false;
+    }
+    public class containsNumber{
+        public static void main(String[] args) {
+            String hasNumber = "benching405";
+            char[] chars = hasNumber.toCharArray();
+            StringBuilder sb = new StringBuilder();
+            for(char c : chars) {
+                if(Character.isDigit(c)){
+                    sb.append(c);
+                }
+            }
+            System.out.println(sb);
+        }
+    }
+
+
 }
--- src/test/java/passwordvalidationTest.java
+++ src/test/java/passwordvalidationTest.java
@@ -1,6 +1,37 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Scanner;
+
+
 public class passwordvalidationTest {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+
+    @Test
+    void passwordValidationTest_whenPasswordLongerThan8Symbols_thenReturnTrue() {
+        //given
+        String password8Letters = "deadcellsenjoyer";
+
+        //when
+        boolean actual = passwordvalidation.passwordIs8letters(password8Letters);
+
+        //then
+        assertTrue(actual);
+
     }
+    @Test
+    void passwordContainsNumberTest_whenpasswordContainsNumber_theReturnTrue() {
+        //given
+        String passwordWithNumber = "riskofrain2enjoyer";
+
+        //when
+        boolean actual = passwordvalidation.passwordIs8letters(passwordWithNumber);
+
+        //then
+        assertTrue(actual);
+    }
+
+    @Test
+    void  passwordHasCapital
 }
